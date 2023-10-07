a3a834e35a8a9958815380f605ed6954eb8ad0b4
kiran chavan <125879292+Kiranchavan05@users.noreply.github.com>
2023-10-07 12:40:56 +0530

    basic

---
+++ app.js
@@ -0,0 +1,14 @@
+const http= require('http');
+
+
+// const serve=function((req,res){
+
+// })
+
+
+
+const server=http.createServer((req,res)=>{
+    console.log('kiran')
+})
+
+server.listen(4000);
